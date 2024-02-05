a9143163a80b820651a90a6069b9cd6ff4c969da
shushuTona <42406967+shushuTona@users.noreply.github.com>
2024-02-06 00:19:01 +0900

    add db container

---
+++ db/insert_tasks_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO tasks VALUES
+    (1, "Waiting", "task1 title", "task1 desc", "2024-02-01 00:00:00"),
+    (2, "Waiting", "task2 title", "task2 desc", "2024-02-01 00:00:00"),
+    (3, "Processing", "task3 title", "task3 desc", "2024-02-01 00:00:00");
