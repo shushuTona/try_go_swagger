e0539fd055f5f53c9ff72cbe829f615f45470d19
shushuTona <42406967+shushuTona@users.noreply.github.com>
2024-02-06 16:48:54 +0900

    update App.tsx

--- src/front/src/App.tsx
+++ src/front/src/App.tsx
@@ -1,20 +1,27 @@
 import './App.css'
+import {Task, Api} from './gen/api'
 import useSWR from 'swr'
 
 const fetcher = ( url: string ) => fetch( url ).then( ( r ) => {
-  console.log("r", r);
-
+    const api = new Api()
+    api.task.getTask()
   return r.json()
 } )
 
 function Profile() {
-  const { data, error, isLoading } = useSWR( 'http://localhost:8081/api/task', fetcher )
-
-  console.log(error);
+  const { data, error, isLoading } = useSWR<Task[]>( 'http://localhost:8085/api/task', fetcher )
 
   if ( error ) return <div>failed to load</div>
   if ( isLoading ) return <div>loading...</div>
-  return <div>hello {data}!</div>
+  if ( data === undefined ) return <div>task is empty</div>
+
+  return <ul>{data.map((item) => (
+    <li key={item.taskId}>
+        <div>
+            <span>{item.title}</span>：<span>{item.desc}</span>
+        </div>
+    </li>
+  ))}</ul>
 }
 
 function App() {
